7cbb029e6fd335d48f8804f331d5450efbd74256
Jacob Ogle <jakeogle@proton.me>
2022-07-07 08:13:52 -0400

    init commit

---
+++ cmd/root.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "ClassTrack CLI",
+	Short: "ClassTrack provides a terminal interface for logging tasks",
+	Long:  ``, //TODO -- Update
+	Run: func(cmd *cobra.Command, args []string) {
+		// Do Stuff Here
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"hwgui/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
